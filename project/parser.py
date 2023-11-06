6b46b499f5672f6c1aabcab8b823960b5eef1d4a
Nikita <109899745+vvetta@users.noreply.github.com>
2023-11-06 18:22:13 +0300

    add refresh and check rows funcs

--- project/parser.py
+++ project/parser.py
@@ -2,6 +2,10 @@ import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 logging.basicConfig(level=logging.INFO, filename="parser.log", filemode="w",
@@ -12,7 +16,9 @@ logger = logging.getLogger("parserLogger")
 def loggerDecorator(func):
     def wrapper(*args, **kwargs):
         logger.info(f"Запущена функция '{func.__name__}'.")
+
         value = func(*args, **kwargs)
+
         logger.info("Функция завершила свою работу.")
         return value
     return wrapper
@@ -26,11 +32,50 @@ def init_parser() -> WebDriver:
 
     return driver
 
+
+@loggerDecorator
+def __check_presence_of_rows(driver: WebDriver) -> bool:
+    """Проверяет наличие всех строк на странице."""
+
+    row_class_name = "tpRow"
+    load_wait_time = 60
+
+    try:
+        WebDriverWait(driver, load_wait_time).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, row_class_name)))
+
+        return True
+
+    except TimeoutException:
+        logger.warning("Не удаётся загрузить все элементы, которые находятся на странице. \
+                Перезагрузка страницы!")
+
+        return False
+
+
+@loggerDecorator
+def _refresh_page(driver: WebDriver) -> WebDriver:
+    """Перезагружает актуальную страницу."""
+
+    driver.refresh()
+
+    if __check_presence_of_rows(driver) == False:
+        exit(1) # Нужно придумать, что делать, если элементы не смогли загрузиться
+                # даже после перезагрузки.
+
+    return driver
+
+
 @loggerDecorator
 def load_page(driver: WebDriver, url: str) -> WebDriver:
     """Функция загружающая страницу."""
 
     driver.get(url=url)
+
+    if __check_presence_of_rows(driver) == False:
+        driver = _refresh_page(driver)
+
+    logger.info("Страница была успешно загружена.")
 
     return driver
 
