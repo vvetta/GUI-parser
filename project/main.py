453e062d80e66136a19f17f91b06750011254f26
Nikita <109899745+vvetta@users.noreply.github.com>
2023-11-06 16:43:19 +0300

    initial commit

---
+++ project/main.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def main() -> None:
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ project/parser.py
@@ -0,0 +1,15 @@
+from typing import NamedTuple
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
+
+    def init_parser():
+        """Функция инициализирующая парсер. """
+        pass
+
+
+    def load_page(driver: WebDriver, url: str):
+        """Функция загружающая страницу."""
+        pass
+
